fd33333466c43e12fc6939a32498c684d30eb55d
Dipendra Singh <dipendra1309@gmail.com>
2025-08-02 08:12:49 +0530

    tesing page created

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -22,3 +22,11 @@ def details(request,id):
 def main(request):
     template=loader.get_template('main.html')
     return HttpResponse(template.render())
+
+def testing(request):
+    template=loader.get_template('template.html')
+    fruits=['apple','banana','orange','dragonfruit' ]
+    context={
+        'fruits':fruits,
+    }
+    return HttpResponse(template.render(context,request))
