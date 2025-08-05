011eeaf24a963f74eddaf52ee5f901573dd9c1c6
Dipendra Singh <dipendra1309@gmail.com>
2025-08-05 18:48:29 +0530

    a complete admin page with with database connectivity

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Member(models.Model):
+
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
   phonenumber=models.IntegerField(null=True)
   joined_date=models.DateField(null=True)
+
+  def __str__(self):
+    return f"{self.firstname} {self.lastname}"
